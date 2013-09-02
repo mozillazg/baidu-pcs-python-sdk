2fc70d4f71e6b1925873a76e695bd60d1782f48f
Mozillazg <opensource.mozillazg@gmail.com>
2013-09-03 00:45:46 +0800

    rename quota -> info, add upload

--- pcs/api.py
+++ pcs/api.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from urllib import urlencode
+import os
+import pdb
+
 import requests
 
 api_template = 'https://pcs.baidu.com/rest/2.0/pcs/{0}'
@@ -10,7 +14,8 @@ class PCS(object):
         self.access_token = access_token
         self.api_template = api_template
 
-    def quota(self):
+    def info(self):
+        """获取当前用户空间配额信息."""
         api = self.api_template.format('quota')
         params = {
             'method': 'info',
@@ -21,6 +26,21 @@ class PCS(object):
         # if response.ok:
         #     pass
 
+    def upload(self, online_path, file_content, ondup=None):
+        """上传单个文件（<2G）.
+        online_path 必须以 /apps/开头
+        """
+        params = {
+            'method': 'upload',
+            'access_token': self.access_token,
+            'path': online_path,
+            'ondup': ondup or ''
+        }
+        api = '%s?%s' % (self.api_template.format('file'), urlencode(params))
+        files = {'file': file_content}
+        response = requests.post(api, files=files)
+        return response.json()
+
 
 def main():
     api_key = '6Uc87wOtAB2VdaSXUpe5z8IW'
@@ -28,7 +48,10 @@ def main():
     access_token = '3.3f56524f9e796191ce5baa84239feb15.2592000.1380728222.'
     access_token += '570579779-1274287'
     pcs = PCS(access_token)
-    print pcs.quota()
+    print pcs.info()
+    with open('api.py', 'rb') as f:
+        print pcs.upload('/apps/test_sdk/api.py', f.read())
+    print pcs.info()
 
 if __name__ == '__main__':
     main()
