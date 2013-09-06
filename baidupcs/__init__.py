f84bdecde28855ee86da29c1cfff97ce52e3c2b3
Mozillazg <opensource.mozillazg@gmail.com>
2013-09-06 22:18:09 +0800

    update __init__.py

--- baidupcs/__init__.py
+++ baidupcs/__init__.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""百度个人云存储（PCS）Python SDK"""
+
+__title__ = 'baidupcs'
+__version__ = '0.1.0'
+__author__ = 'mozillazg'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2013 mozillazg'
+
 from api import PCS
