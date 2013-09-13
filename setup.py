88166889887e1fcac2eec63cb83b8fe96cf22c4e
Mozillazg <opensource.mozillazg@gmail.com>
2013-09-13 15:56:08 +0800

    0.3.0 released

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 __title__ = 'baidupcs'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 __author__ = 'mozillazg'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2013 mozillazg'
