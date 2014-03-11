31e0c0cb87ece72f69bf842dcefb683e7bf3a052
fy <fy0748@gmail.com>
2014-03-11 12:22:26 +0800

    修复了windows下py3无法安装（编码错误）的问题

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ packages = [
 
 
 def long_description():
-    return open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read()
+    return open('README.rst', encoding= 'utf-8').read() + '\n\n' + open('CHANGELOG.rst', encoding= 'utf-8').read()
 
 setup(
     name='baidupcs',
